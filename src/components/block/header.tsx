cae784af150e384bd0c130e553c8896c71f26877
Almas Sapar <saparovalmas0@gmail.com>
2024-10-12 18:36:02 -0700

    feat: block cta fix

--- src/components/block/header.tsx
+++ src/components/block/header.tsx
@@ -24,17 +24,34 @@ export const Header: React.FC<HeaderProps> = ({
 }) => {
   const isMobile = useMobile();
   const state = active || isMobile ? "visible" : "hidden";
-
   return (
     <motion.div
-      className="absolute bottom-8 bg-white/80 flex gap-2.5 items-center whitespace-nowrap shadow-lg shadow-neutral-600/5 text-xs lg:text-sm backdrop-blur-sm left-1/2 py-1 pr-2.5 pl-1.5 rounded-full"
-      initial="visible"
-      animate={state}
+      className="flex items-center gap-1 absolute bottom-8 left-1/2"
+      initial="hidden"
+      whileInView={state}
+      viewport={{ once: true }}
       variants={HEADER_ANIMATION}
       transition={HEADER_TRANSITION}
     >
-      <Icon letter={letter?.at(0)} color={color} />
-      {hook}
+      <div className="bg-white/80 flex gap-2.5 items-center whitespace-nowrap shadow-lg shadow-neutral-600/5 text-xs lg:text-sm backdrop-blur-sm py-1 pr-2.5 pl-1.5 rounded-full">
+        <Icon letter={letter?.at(0)} color={color} />
+        {hook}
+      </div>
+      <div className="bg-white/80 size-8 justify-center leading-5 text-neutral-500 text-2xl flex items-center shadow-lg shadow-neutral-600/5 backdrop-blur-sm rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="m9 18 6-6-6-6" />
+        </svg>
+      </div>
     </motion.div>
   );
 };

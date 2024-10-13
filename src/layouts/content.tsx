5cdb92ecbf0e7ece4d088465f76efef758e6ad3d
Almas Sapar <saparovalmas0@gmail.com>
2024-10-12 17:47:03 -0700

    feat: project cta

--- src/layouts/content.tsx
+++ src/layouts/content.tsx
@@ -1,20 +1,35 @@
 import clsx from "clsx";
 
-export const Title: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
+interface Title extends React.HTMLAttributes<HTMLDivElement> {
+  href?: string;
+}
+
+export const Title: React.FC<Title> = ({
   className,
   children,
+  href,
   ...props
 }) => {
   return (
-    <h2
-      className={clsx(
-        "text-4xl md:text-8xl font-medium md:leading-[90px] mb-6 tracking-tighter text-balance md:mb-auto text-neutral-500",
-        className
+    <div className="flex flex-col gap-3 md:gap-5 grow md:mb-auto">
+      <h2
+        className={clsx(
+          "text-4xl md:text-8xl font-medium md:leading-[90px] tracking-tighter text-balance 2xl:mr-96 text-neutral-500",
+          className
+        )}
+        {...props}
+      >
+        {children}
+      </h2>
+      {href && (
+        <a
+          href={href}
+          className="bg-neutral-50 rounded-full text-sm text-black hover:text-neutral-500 duration-200 items-center flex gap-2 border border-neutral-300 py-1.5 px-2.5 mr-auto mb-8"
+        >
+          Visit link <span className="font-mono h-4">↗</span>
+        </a>
       )}
-      {...props}
-    >
-      {children}
-    </h2>
+    </div>
   );
 };
 
